62a21bbc4d2952d05ec9f59d46643474afb01b71
Tri Thai <thaixtri@gmail.com>
2013-06-22 18:31:07 -0500

    statics

--- ShapesDemo2D.java
+++ ShapesDemo2D.java
@@ -2,3 +2,19 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
 import javax.swing.*;
+
+public class ShapesDemo2D extends JApplet{
+    final static int maxCharHeight = 15;
+    final static int minFontSize = 6;
+    
+    final static Color bg = Color.white;
+    final static Color fg = Color.black;
+    final static Color red = Color.red;
+    final static Color white = Color.white;
+
+    final static BasicStroke stroke = new BasicStroke(2.0f);
+    final static BasicStroke wideStroke = new BasicStroke(8.0f);
+    
+    final static float dash1[] = {10.0};
+    final static BasicStroke dashed = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, dash1, 0.0f); 
+}
